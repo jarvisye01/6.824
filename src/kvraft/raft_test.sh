afd2b34ec8080613cdc39a4be8c2a95aa4791209
jarvisye <972763217@qq.com>
2023-12-17 00:21:36 +0800

    Fix log.

--- src/kvraft/raft_test.sh
+++ src/kvraft/raft_test.sh
@@ -4,5 +4,5 @@ testFunc=$1
 
 for num in {1..100}
 do
-    SERVER_VERBOSE=6 RAFT_VERBOSE=6 go test -run "$1"
+    go test -run "$1"
 done
--- src/raft/util.go
+++ src/raft/util.go
@@ -36,11 +36,12 @@ var (
 )
 
 const (
-	TraceLevel = 0
-	DebugLevel = 1
-	WarnLevel  = 2
-	InfoLevel  = 3
-	ErrorLevel = 4
+	TraceLevel = 5
+	DebugLevel = 4
+	WarnLevel  = 3
+	InfoLevel  = 2
+	ErrorLevel = 1
+	EmptyLevel = 0
 )
 
 func init() {
@@ -51,7 +52,7 @@ func init() {
 
 func getVerbosity() int {
 	v := os.Getenv("RAFT_VERBOSE")
-	level := 0
+	level := EmptyLevel
 	if v != "" {
 		var err error
 		level, err = strconv.Atoi(v)
@@ -71,25 +72,25 @@ func logf(t logTopic, format string, v ...interface{}) {
 }
 
 func Tracef(t logTopic, format string, v ...interface{}) {
-	if logVerbosity <= TraceLevel {
+	if logVerbosity >= TraceLevel {
 		logf(t, format, v...)
 	}
 }
 
 func Debugf(t logTopic, format string, v ...interface{}) {
-	if logVerbosity <= DebugLevel {
+	if logVerbosity >= DebugLevel {
 		logf(t, format, v...)
 	}
 }
 
 func Warnf(t logTopic, format string, v ...interface{}) {
-	if logVerbosity <= WarnLevel {
+	if logVerbosity >= WarnLevel {
 		logf(t, format, v...)
 	}
 }
 
 func Infof(t logTopic, format string, v ...interface{}) {
-	if logVerbosity <= InfoLevel {
+	if logVerbosity >= InfoLevel {
 		logf(t, format, v...)
 	}
 }
